260da951d46cda78e1f62fd5e3633484cc475f43
AydinTokuslu <aydintokuslu78@gmail.com>
2023-12-05 09:04:00 +0100

    exercise10

---
+++ exercise10.py
@@ -0,0 +1,14 @@
+# Write a function called user_name that generates a username
+# from the user’s email. The code should ask the user to input an
+# email and the code should return everything before the @ sign
+# as their user_name. For example, if someone enters
+# ben@gmail.com, the code should return ben as their user_name
+
+def user_name():
+    email = input("please enter your email addresses: ")
+    email = email.split("@")
+    email_name = email[0]
+    return email_name.upper()
+
+#print(user_name())
+print(f"Your user name is {user_name()}")
