4873df53e6a1261b52d3fff6ba257cb7d9f84ecc
AydinTokuslu <aydintokuslu78@gmail.com>
2023-12-08 09:12:37 +0100

    exercise27

---
+++ exercise27.py
@@ -0,0 +1,17 @@
+# A school has asked you to write a program that will calculate
+# teachers' salaries. The program should ask the user to enter the
+# teacher’s name, the number of periods taught in a month,
+# and the rate per period. The monthly salary is calculated by
+# multiplying the number of periods by the monthly rate.
+# The current monthly rate per period is $20. If a teacher has
+# more than 100 periods in a month, everything above 100 is
+# overtime. Overtime is $25 per period. For example, if a teacher
+# has taught 105 periods, their monthly gross salary should be
+# 2,125. Write a function called your_salary that calculates a
+# teacher’s gross salary. The function should return the
+# teacher’s name, periods taught, and gross salary. Here is
+# how you should format your output:
+# Teacher: John Kelly,
+# Periods: 105
+# Gross salary:2,125
+
