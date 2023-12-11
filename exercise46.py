ddbf626d763422e088f42499f941049bbc3a07d3
AydinTokuslu <aydintokuslu78@gmail.com>
2023-12-11 10:24:21 +0100

    exercise46

--- exercise46.py
+++ exercise46.py
@@ -3,3 +3,15 @@
 # elements are the same. If the elements are the same, the function
 # should return True. If not, it should return False. For example,
 # [‘Mary’, ‘Mary’, ‘Mary’] should return True
+
+def all_the_same(s):
+    for i in range(0, len(s)):
+        if s[i] == s[i+1]:
+           return True
+        else:
+            return False
+
+
+
+arguments = ["Mary", "Mary", "Kary"]
+print(all_the_same(arguments))
