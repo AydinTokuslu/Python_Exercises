76ce4be7598a7fb13a0dee712b95a6373fa04412
AydinTokuslu <aydintokuslu78@gmail.com>
2023-12-11 09:23:24 +0100

    exercise43

--- exercise43.py
+++ exercise43.py
@@ -10,3 +10,20 @@
 # Hate and Peace will be ignored because they have at least one
 # uppercase letter.
 
+def multiply_words(s):
+    sum = 1
+    s_new = ""
+    s = s.split(" ")
+    for i in s:
+        if i.islower():
+            sum *= len(i)
+            s_new += i + " "
+            s_new += str(len(i)) + " "
+        else:
+            continue
+    return f"{sum} - {s_new}"
+
+
+#s = "love live and laugh"
+s = "Hate war love Peace"
+print(multiply_words(s))
