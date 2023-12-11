fe14be09d61a1ce6625e383f2e5b8bc514b86da8
AydinTokuslu <aydintokuslu78@gmail.com>
2023-12-11 09:58:50 +0100

    exercise44

--- exercise44.py
+++ exercise44.py
@@ -3,3 +3,15 @@
 # to input their calories intake for any number of days and once they
 # hit ‘done’ it should calculate and return the average intake.
 
+def average_calories():
+    calories = []
+    while True:
+        calori = input("please enter the calorie, or enter 'done' to exit : ")
+        if calori == "done":
+            break
+        else:
+            calories.append(int(calori))
+    return f"Average intake is {sum(calories)/len(calories)}"
+
+print(average_calories())
+
