58a5f2a2da4d1d00747a4e3703c514c5e4825024
AydinTokuslu <aydintokuslu78@gmail.com>
2023-12-11 10:12:05 +0100

    exercise46

---
+++ exercise46.py
@@ -0,0 +1,5 @@
+# Create a function called all_the_same that takes one
+# argument, a string, a list, or a tuple and checks if all the
+# elements are the same. If the elements are the same, the function
+# should return True. If not, it should return False. For example,
+# [‘Mary’, ‘Mary’, ‘Mary’] should return True
