bd478856a536649e187ad91ca95c73916e3e9d26
AydinTokuslu <aydintokuslu78@gmail.com>
2023-12-11 10:24:58 +0100

    exercise47

---
+++ exercise47.py
@@ -0,0 +1,5 @@
+# str1 = "the love is real"
+# Write a function called read_backwards that takes a string as
+# an argument and reverses it. For example, the string above
+# should return: "real is love the"
+
