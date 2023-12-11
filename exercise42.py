d79823e946552251c22d0906c5bbe9492f45b410
AydinTokuslu <aydintokuslu78@gmail.com>
2023-12-11 09:00:36 +0100

    exercise42

--- exercise42.py
+++ exercise42.py
@@ -3,3 +3,26 @@
 # calculator should take input from users. The calculator should be
 # able to handle ZeroDivisionError, NameError, and ValueError.
 
+def calculator():
+    try:
+        num1 = int(input("please enter a number : "))
+        opt = input("please select an operator (+ - / *): ")
+        num2 = int(input("please enter another number for calculation : "))
+        if opt not in ['+', '-', '/', '*'] or len(opt)>1:
+            print("please enter a correct operator (+ - / *) ")
+    except ValueError:
+        print("please enter a valid number ")
+    except ZeroDivisionError:
+        print("You cannot divide a number by zero.Try again ")
+    else:
+        if opt == "+":
+            return f"answer is {num1+num2}"
+        elif opt == "_":
+            return f"answer is {num1-num2}"
+        elif opt == "/":
+            return f"answer is {num1/num2}"
+        elif opt == "*":
+            return f"answer is {num1*num2}"
+    return "Try again"
+
+print(calculator())
