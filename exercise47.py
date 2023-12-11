4ff883ee63903ca5fed33b94c7ed9eab54eefd23
AydinTokuslu <aydintokuslu78@gmail.com>
2023-12-11 10:28:45 +0100

    exercise47

--- exercise47.py
+++ exercise47.py
@@ -3,3 +3,11 @@
 # an argument and reverses it. For example, the string above
 # should return: "real is love the"
 
+def read_backwards(str):
+    new_str = str.split()
+    for i in reversed(new_str):
+        print(i, end=" ")
+
+
+str1 = "the love is real"
+read_backwards(str1)
