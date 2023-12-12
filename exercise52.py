745920f681d8c370275d60f55484c3b8261703f4
AydinTokuslu <aydintokuslu78@gmail.com>
2023-12-12 21:01:03 +0100

    exercise52

--- exercise52.py
+++ exercise52.py
@@ -3,4 +3,13 @@
 # lower letters in the string. For example, ‘LovE’ should return
 # [1,2].
 
-def index_position():
+
+def index_position(str):
+    index_num = []
+    for i in str:
+        if i.islower():
+            index_num.append(str.index(i))
+    return index_num
+
+x = "LovE"
+print(index_position(x))
