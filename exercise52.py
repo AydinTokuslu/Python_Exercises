140fa9ff5392075666375206b7854989cef590be
AydinTokuslu <aydintokuslu78@gmail.com>
2023-12-12 20:54:00 +0100

    exercise52

---
+++ exercise52.py
@@ -0,0 +1,5 @@
+# Write a function called index_position. This function takes a
+# string as a parameter and returns the positions or indexes of all
+# lower letters in the string. For example, ‘LovE’ should return
+# [1,2].
+
