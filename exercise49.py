cdb2d028b0570411d952051226cbe753b26adb1d
AydinTokuslu <aydintokuslu78@gmail.com>
2023-12-12 20:27:31 +0100

    exercise49

--- exercise49.py
+++ exercise49.py
@@ -5,3 +5,12 @@
 # in a form of a dictionary. The string above should return:
 # {'Hi': 2, 'my': 2, 'name': 4, 'is': 2, 'Richard': 7}
 
+def string_length(str):
+    new_str = {}
+    str = str.split(" ")
+    for i in str:
+        new_str[i] = len(i)
+    return new_str
+
+s = 'Hi my name is Richard'
+print(string_length(s))
