f848cf9ada62f6972d2d00833f348d3fafb8c3d1
AydinTokuslu <aydintokuslu78@gmail.com>
2023-12-12 21:08:06 +0100

    exercise53

--- exercise53.py
+++ exercise53.py
@@ -4,3 +4,13 @@
 # argument: list1 = [3, 67, 87, 9, 2]. Your code should return the
 # number in this exact format: 9,877,632 as the largest number.
 
+def largest_number(list):
+    number = ""
+    max = 0
+    for i in list:
+        number += str(i)
+    print(number)
+
+list1 = [3, 67, 87, 9, 2]
+print(largest_number(list1))
+
