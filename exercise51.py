6c91acce11c0831992ae4912dae26baada2bca7b
AydinTokuslu <aydintokuslu78@gmail.com>
2023-12-12 20:50:17 +0100

    exercise51

---
+++ exercise51.py
@@ -0,0 +1,10 @@
+# Write a function called difference that takes two lists as
+# arguments. This function should return all the elements that are
+# in list a but not in list b and all the elements in list b not in list
+# a. For example:
+# list1 = [1, 2, 4, 5, 6]
+# list2 = [1, 2, 5, 7, 9]
+# should return:
+# [4, 6, 7, 9]
+# Use list comprehension in your function.
+
