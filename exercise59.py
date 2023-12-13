11f0aa17b44f2c8a22bfa9e0f40ababac07c18d5
AydinTokuslu <aydintokuslu78@gmail.com>
2023-12-13 09:19:29 +0100

    exercise59

--- exercise59.py
+++ exercise59.py
@@ -9,3 +9,18 @@
 # prompt the user to enter another password. The code should
 # only stop once the user enters a valid password. (use while loop).
 
+def password_validator():
+    errors = []
+    while True:
+        password = input("please enter your minimum 8 character password.\nPassword should have at least one upper letter, "
+                     "one lower letter, and one number")
+        if len(password) < 8:
+            print("Your password should be 8 characters")
+        elif password.isdigit():
+            print("Your password should have at least one upper letter, one lower letter")
+        elif password.islower():
+            print("Your password should have at least one upper letter, one number")
+        elif password.isupper():
+            print("Your password should have at least one lower letter, one number")
+        else:
+            return f"Your password is : {password}"
