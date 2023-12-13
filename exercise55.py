81a983c6257cea14c44a2bd933ef333a33fafa0d
AydinTokuslu <aydintokuslu78@gmail.com>
2023-12-13 08:32:41 +0100

    exercise55

--- exercise55.py
+++ exercise55.py
@@ -2,3 +2,15 @@
 # repeated name in the following list.
 # name = ["John", "Peter", "John", "Peter", "Jones", "Peter"]
 
+def repeated_name(list):
+    repeat_list = {}
+    for i in list:
+        if i not in repeat_list:
+            repeat_list[i]=1
+        else:
+            repeat_list[i]+=1
+    print(repeat_list)
+    return f"most repeated name is : {max(repeat_list)} and repeat number is : {max(repeat_list.values())}"
+
+name = ["John", "Peter", "John", "Peter", "Jones", "Peter"]
+print(repeated_name(name))
