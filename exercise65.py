45435eb5d776de36590298ea1d4c9a4abb16b48f
AydinTokuslu <aydintokuslu78@gmail.com>
2023-12-14 15:03:38 +0100

    exercise65

--- exercise65.py
+++ exercise65.py
@@ -8,3 +8,19 @@
 # the list is [1, 2, 4, 6, 7, 7] and the integer is 8, your code should
 # return [8, 8, 8, 8, 8, 8] because 8 is not in the list.
 
+def find_index(list1,num1):
+    indexes = []
+    if num1 in list1:
+        for i in list1:
+            if i == num1:
+                indexes.append(list1.index(i))
+        print(indexes)
+    else:
+        for i in range(len(list1)):
+            indexes.append(num1)
+        print(indexes)
+
+
+list = [1, 2, 4, 6, 7, 7]
+num = 7
+find_index(list,num)
