2c1f50bd644a0b419bbdc6c347ae0b36cccf8df3
AydinTokuslu <aydintokuslu78@gmail.com>
2023-12-14 14:44:18 +0100

    exercise64

---
+++ exercise64.py
@@ -0,0 +1,7 @@
+# Write a function called check_pangram that takes a string
+# and checks if it is a pangram. A pangram is a sentence that
+# contains all the letters of the alphabet. If it is a pangram,
+# the function should return True, otherwise, return False. The
+# following sentence is a pangram so it should return True:
+# 'the quick brown fox jumps over a lazy dog'
+
