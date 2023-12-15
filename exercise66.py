2f554eeedd2cf390cabca8150770ea49ae3ad253
AydinTokuslu <aydintokuslu78@gmail.com>
2023-12-15 11:23:13 +0100

    exercise66

--- exercise66.py
+++ exercise66.py
@@ -3,3 +3,15 @@
 # appears in the string. For example, ‘hello’ should return:
 # {‘h’:1,’e’: 1,’l’:2, ‘o’:1}.
 
+def count(str):
+    element = {}
+    for i in str:
+        if i not in element.keys():
+            element[i] = 1
+        else:
+            element[i] += 1
+    return element
+
+
+s = "hello"
+print(count(s))
