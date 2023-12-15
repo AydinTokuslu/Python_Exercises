c493fa646eb139edaac56960d581168b2b98c064
AydinTokuslu <aydintokuslu78@gmail.com>
2023-12-15 11:29:29 +0100

    exercise67

--- exercise67.py
+++ exercise67.py
@@ -5,3 +5,18 @@
 # as one. For example, ‘saas’ should return 1 vowel. Your code
 # should count lowercase and uppercase vowels.
 
+def count_the_vowels(str):
+    vowels = ["a","e","i","o","u"]
+    vowels_list = []
+    for i in str:
+        if i in vowels:
+            if i not in vowels_list:
+                vowels_list.append(i)
+        else:
+            continue
+    return len(vowels_list)
+
+#s = "hello"
+s = "aslihan"
+#s = "saas"
+print(count_the_vowels(s))
