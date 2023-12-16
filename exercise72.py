c1e9e10675bca84d525ad62074cabb6dc6304976
AydinTokuslu <aydintokuslu78@gmail.com>
2023-12-16 13:04:03 +0100

    exercise72

---
+++ exercise72.py
@@ -0,0 +1,5 @@
+# Create a function called words_with_vowels, this function
+# takes a string of words and returns a list of only words that
+# have vowels in them. For example, ‘You have no rhythm’ should
+# return [‘You’,’have’, ‘no’].
+
