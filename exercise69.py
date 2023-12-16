deb5115a132133781a1580f729689b18613d23a1
AydinTokuslu <aydintokuslu78@gmail.com>
2023-12-16 12:34:36 +0100

    exercise69

---
+++ exercise69.py
@@ -0,0 +1,7 @@
+# list = [1, 2, 3, 5, 6, 7, 9, 11, 12, 23, 14, 15, 17,
+#  18, 19, 20, 21, 22, 24, 25, 26, 27, 28, 31]
+# Write a function called missing_numbers that takes a list of
+# sequence of numbers and finds the missing numbers in the
+# sequence. The list above should return:
+# [4, 8, 10, 13, 16, 29, 30]
+
