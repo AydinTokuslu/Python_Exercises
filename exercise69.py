183d1e2a112b719c18d184decd2e31555aa459d2
AydinTokuslu <aydintokuslu78@gmail.com>
2023-12-16 12:35:49 +0100

    exercise69

--- exercise69.py
+++ exercise69.py
@@ -5,3 +5,9 @@
 # sequence. The list above should return:
 # [4, 8, 10, 13, 16, 29, 30]
 
+def missing_numbers(list):
+    new_list = []
+
+
+list = [1, 2, 3, 5, 6, 7, 9, 11, 12, 23, 14, 15, 17, 18, 19, 20, 21, 22, 24, 25, 26, 27, 28, 31]
+print(missing_numbers(list))
