123b20a07a148c2bd721037ba0a48ee8e455d943
AydinTokuslu <aydintokuslu78@gmail.com>
2023-12-16 16:21:58 +0100

    exercise72

--- exercise72.py
+++ exercise72.py
@@ -3,3 +3,19 @@
 # have vowels in them. For example, ‘You have no rhythm’ should
 # return [‘You’,’have’, ‘no’].
 
+def words_with_vowels(str):
+    vowels = ["a", "e", "i", "o", "u"]
+    str = str.split()
+    new_list = []
+    for i in str:
+        for j in vowels:
+            if j in i:
+                if i not in new_list:
+                    new_list.append(i)
+    print(new_list)
+
+
+
+
+str = "You have no rhythm"
+words_with_vowels(str)
