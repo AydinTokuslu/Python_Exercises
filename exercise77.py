de27ea4c8db7a932c409256493e6f0d286cd0be3
AydinTokuslu <aydintokuslu78@gmail.com>
2023-12-22 07:04:02 +0100

    exercise77

--- exercise77.py
+++ exercise77.py
@@ -12,3 +12,23 @@
 # analogously to printf format strings in C, e.g. "spam=%s
 # eggs=%d" % ("blah", 2) evaluates to "spam=blah eggs=2".
 
+def analyse_string(str):
+    special_characters = "#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
+    special_char = ""
+    total_char_count = 0
+    for i in str:
+        if i in special_characters:
+            special_char += i
+
+    words = str.split()
+    num_words = len(words)
+
+    for word in words:
+        total_char_count += len(word)
+
+    return f"special characters: {len(special_characters)},\nwords: {num_words},\ntotal characters: {total_char_count}"
+
+
+str = "Python has a string format operator %. This functions analogously to printf format strings in C, e.g. 'spam=%s eggs=%d' % ('blah', 2) evaluates to 'spam=blah eggs=2'."
+
+print(analyse_string(str))
