17637040a224944a5c4dc0d28ded267a0cbdca1c
AydinTokuslu <aydintokuslu78@gmail.com>
2023-12-24 13:11:14 +0100

    exercise80

---
+++ exercise80.py
@@ -0,0 +1,9 @@
+# Create a code that extracts data from a website. You will extract a
+# table from the website. And from that table you will extract columns
+# about the data types in Python and their mutability. You will extract
+# information from the following website:
+
+# https://en.wikipedia.org/wiki/Python_(programming_language)
+
+# The following table (next page) is what you will extract from the website.
+
