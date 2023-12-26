2d39742c9cc14dbc656688697326519746bb8549
AydinTokuslu <aydintokuslu78@gmail.com>
2023-12-26 12:16:42 +0100

    exercise82

--- exercise82.py
+++ exercise82.py
@@ -4,6 +4,19 @@
 # is being searched for. Use binary search (iterative Method).
 # list1 = [12, 34, 56, 78, 98, 22, 45, 13]
 
+def search_binary(list1,x):
+    low = 0
+    high = len(list1)-1
+    while low <= high:
+        mid_index = (high + low) // 2
+        if list1[mid_index] == x:
+            return mid_index
+        elif list1[mid_index] > x :
+            high = mid_index -1
+        elif list1[mid_index] < x :
+            low = mid_index + 1
+    return "no element"
+
 
 
 list1 = [12, 34, 56, 78, 98, 22, 45, 13]
