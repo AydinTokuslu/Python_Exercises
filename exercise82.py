d6e4c29e2a52688ef3a0d1068700d504c9d5d7c0
AydinTokuslu <aydintokuslu78@gmail.com>
2023-12-26 12:20:26 +0100

    exercise82

--- exercise82.py
+++ exercise82.py
@@ -22,3 +22,10 @@ def search_binary(list1,x):
 list1 = [12, 34, 56, 78, 98, 22, 45, 13]
 list1 = sorted(list1)
 print(list1)
+number = 34
+
+results = search_binary(list1,number)
+if results == "no element":
+    print("Element is not in the list")
+else:
+    print(f"The element index is {results}")
