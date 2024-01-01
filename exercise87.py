04c0d4185151875013fedaa0ae1d6cb6737c3afc
AydinTokuslu <aydintokuslu78@gmail.com>
2024-01-01 19:26:18 +0100

    exercise87

--- exercise87.py
+++ exercise87.py
@@ -5,3 +5,16 @@ tura=0
 
 para_yuzeyi=["tura","yazi"]
 atilcak_para_sayisi=int(input("kaç kere atmak istiyorsunuz : "))
+
+while atilcak_para_sayisi>0:
+    para_ustu=random.choice(para_yuzeyi)
+    print(para_ustu)
+    if para_ustu=="tura" or para_ustu=="Tura":
+        tura+=1
+        print("tura geldi!!")
+    else:
+        yazi+=1
+        print("yazi geldi!!!")
+    atilcak_para_sayisi-=1
+
+print("Tura sayısı : {}\nYazi sayısı : {}".format(yazi,tura))
