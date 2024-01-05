b356a486b0b5636d751d1ddd35a7459eeebba7f2
AydinTokuslu <aydintokuslu78@gmail.com>
2024-01-05 17:34:43 +0100

    exercise90

--- exercise90.py
+++ exercise90.py
@@ -1,1 +1,11 @@
 ## tam sayı bölenler
+
+sayi=int(input("bir sayı giriniz : "))
+def tamSayiBolenleriAl(sayi):
+    tamBolenler = []
+    for i in range(1, sayi):
+        if sayi%i == 0:
+            tamBolenler.append(i)
+    return tamBolenler
+
+print(tamSayiBolenleriAl(sayi))
