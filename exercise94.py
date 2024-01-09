7056aa3bfe49d244a22c3c8386b8cd780ac6a1ec
AydinTokuslu <aydintokuslu78@gmail.com>
2024-01-09 17:31:58 +0100

    exercise94

--- exercise94.py
+++ exercise94.py
@@ -20,13 +20,13 @@ if secim == "s":
             else:
                 sahip_olunan_para-=cekilmek_istenen_tutar
                 print("Hesabınızdan {} tl para çekilmiştir. Teşekkürler. Yeni Bakiyeniz : ".format(cekilmek_istenen_tutar, sahip_olunan_para))
-        elif islem == 2:
+        if islem == 2:
             yatirilmek_istenen_tutar = int(input("Yatırmak istediğiniz para tutarını giriniz : "))
             sahip_olunan_para+=yatirilmek_istenen_tutar
             print("Hesabınıza {} tl para yatırılmıştır. Teşekkürler. Yeni Bakiyeniz : ".format(yatirilmek_istenen_tutar, sahip_olunan_para))
-        elif islem == 3:
+        if islem == 3:
             print("Hesap Bakiyeniz:\n*****Hesap Sahibi : Aydin T*****\nSahip olunan para miktarı: {}".format(sahip_olunan_para))
-        else:
+        if islem == 4:
             print("Kartınız iade edilmektedir. Gene bekleriz.")
             break
 
