acd191a275dcda841f8067d249e64d617ccffa88
AydinTokuslu <aydintokuslu78@gmail.com>
2024-01-09 17:16:56 +0100

    exercise94

--- exercise94.py
+++ exercise94.py
@@ -14,6 +14,15 @@ if secim == "s":
         
         Yapmak istediğiniz işlemin numarasını giriniz : 
         """))
+        if islem == 1:
+            cekilmek_istenen_tutar = int(input("Çekmek istediğiniz para tutarını giriniz : "))
+            if sahip_olunan_para < cekilmek_istenen_tutar:
+                print("Bu işle  geçersizdir. Yeterli bakiyeniz bulunmamaktadır.")
+            else:
+                sahip_olunan_para-=cekilmek_istenen_tutar
+        elif islem == 2:
+            yatirilmek_istenen_tutar = int(input("Yatırmak istediğiniz para tutarını giriniz : "))
+            sahip_olunan_para+=yatirilmek_istenen_tutar
 
 else:
     print("ATM'den ayrıldınız")
